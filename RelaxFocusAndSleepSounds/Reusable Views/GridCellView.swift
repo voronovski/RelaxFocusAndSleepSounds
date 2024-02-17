8ad046510569b032ec20a6785f20116491962709
voronovski <avoronovski@gmail.com>
2024-02-17 00:32:01 -0800

    grid view realised

---
+++ RelaxFocusAndSleepSounds/Reusable Views/GridCellView.swift
@@ -0,0 +1,18 @@
+//
+//  GridCellView.swift
+//  RelaxFocusAndSleepSounds
+//
+//  Created by Aleksei Voronovskii on 2/16/24.
+//
+
+import SwiftUI
+
+struct GridCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridCellView()
+}
