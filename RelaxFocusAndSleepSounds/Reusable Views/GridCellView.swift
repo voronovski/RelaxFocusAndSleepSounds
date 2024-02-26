e2ce02dbb02af00eb616a546eb379f9584545870
voronovski <avoronovski@gmail.com>
2024-02-25 16:41:09 -0800

    grid cell view - text description added

--- RelaxFocusAndSleepSounds/Reusable Views/GridCellView.swift
+++ RelaxFocusAndSleepSounds/Reusable Views/GridCellView.swift
@@ -15,26 +15,32 @@ struct GridCellView: View {
     var sound: Sound
     
     var body: some View {
-        ZStack {
-            Image(sound.fileName)
-                .resizable()
-                .frame(width: 100, height: 100)
-                .cornerRadius(20)
-                .shadow(radius: 10)
-                .onTapGesture(count: 1) {
-                    audioManager.startPlayer(sound: sound.fileName)
-                }
-            
-            Image(systemName: sound.isFavorite ? "heart.fill" : "heart")
-                .resizable()
-                .frame(width: 22, height: 22)
-                .offset(x: 28, y: -28)
-                .foregroundStyle(.accent)
-                .onTapGesture {
-                    withAnimation {
-                        dataManager.toggleFavorite(sound: sound)
+        VStack {
+            ZStack {
+                Image(sound.fileName)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                    .onTapGesture(count: 1) {
+                        audioManager.startPlayer(sound: sound.fileName)
                     }
-                }
+                
+                Image(systemName: sound.isFavorite ? "heart.fill" : "heart")
+                    .resizable()
+                    .frame(width: 22, height: 22)
+                    .offset(x: 28, y: -28)
+                    .foregroundStyle(.accent)
+                    .onTapGesture {
+                        withAnimation {
+                            dataManager.toggleFavorite(sound: sound)
+                        }
+                    }
+            }
+            Text(sound.name)
+                .font(.caption)
+                .frame(width: 100)
+                .lineLimit(1)
         }
     }
 }
