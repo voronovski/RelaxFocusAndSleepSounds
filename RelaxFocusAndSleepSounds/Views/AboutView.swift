5d29452925bc2d806799f3232f3d9fdee04564db
voronovski <avoronovski@gmail.com>
2024-03-07 00:55:48 -0800

    AboutView image resize when tapped

--- RelaxFocusAndSleepSounds/Views/AboutView.swift
+++ RelaxFocusAndSleepSounds/Views/AboutView.swift
@@ -11,20 +11,35 @@ struct AboutView: View {
     
     let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
     
+    @State private var tapCount = 0
+    @State private var isImageEnlarged = false
     
     var body: some View {
-        Image("Icon")
-            .resizable()
-            .frame(width: 150, height: 150)
-            .cornerRadius(20)
-        Text("App created by Alex Voronovski, thanks for using!")
-            .multilineTextAlignment(.center)
-            .font(.headline)
-            .padding()
-        Text("Version: \(appVersion)")
-            .font(.subheadline)
+        VStack {
+            Image("Icon")
+                .resizable()
+                .frame(width: isImageEnlarged ? UIScreen.main.bounds.width - 32 : 150, height: isImageEnlarged ? UIScreen.main.bounds.width - 32 : 150)
+                .cornerRadius(isImageEnlarged ? 40 : 20)
+                .onTapGesture {
+                    tapCount += 1
+                    if tapCount == 5 {
+                        withAnimation {
+                            isImageEnlarged.toggle()
+                        }
+                        tapCount = 0
+                    }
+                }
+            
+            Text("App created by Alex Voronovski, thanks for using!")
+                .multilineTextAlignment(.center)
+                .font(.headline)
+                .padding()
+            Text("Version: \(appVersion)")
+                .font(.subheadline)
+        }
     }
 }
+
 
 #Preview {
     AboutView()
